469490cac88a9e322e373698b09d0e77169a32f2
Falko Noe <falkonoe@gmail.com>
2019-11-02 16:18:44 +0100

    Almost working XVal with Keras

--- main.py
+++ main.py
@@ -1,13 +1,73 @@
-import pandas as pd
+# system
 import os.path as ospath
-import numpy as np
 from os import makedirs
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import IsolationForest
-from sklearn.decomposition import PCA, KernelPCA
-from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
+
+# general packages
+import numpy as np
+import pandas as pd
+
+# scikit-learn helpers
+from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import KernelPCA
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 import matplotlib.pyplot as plt
+
+# keras machinery
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.constraints import max_norm
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.utils import class_weight
+from tensorflow import set_random_seed
+
+
+########################################################
+################ Helper functions ######################
+########################################################
+
+def check_for_imbalance(Y):
+    unique, counts = np.unique(Y, return_counts=True)
+    # format output nicely
+    for i, cl in enumerate(unique):
+        print("Class label: {} \tCount: {}".format(int(cl), counts[i]))
+    return class_weight.compute_class_weight('balanced', np.unique(Y), Y)
+
+
+def encode_labels(Y):
+    encoder = LabelEncoder()
+    encoder.fit(Y)
+    return np_utils.to_categorical(encoder.transform(Y))
+
+
+########################################################
+################## define model ########################
+########################################################
+def create_model(npl=100, lrs=3, activation_fct='relu', dropout=0, learning_rate=1e-3):
+    """
+    npl      neurons per layer
+    lrs      number of layers
+    """
+    model = Sequential()
+    model.add(Dense(npl, activation=activation_fct, input_dim=1000))
+    model.add(Dense(units=npl, activation=activation_fct, kernel_constraint=max_norm(3), bias_constraint=max_norm(3)))
+    if dropout > 0:
+        model.add(Dropout(rate=dropout))
+    if lrs > 1:
+        for i in range(lrs-1):
+            model.add(Dense(units=npl, activation=activation_fct, kernel_constraint=max_norm(3), bias_constraint=max_norm(3)))
+            if dropout > 0:
+                model.add(Dropout(rate=dropout))
+    model.add(Dense(units=3, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=learning_rate), metrics=['accuracy'])
+
+    return model
+
+
+########################################################
+#################### main ##############################
+########################################################
 
 
 def plot3clusters(x, y, title, vtitle, out_filename="training_data.png"):
@@ -33,6 +93,14 @@ def perform_data_scaling(x_train, x_test):
 
 
 def main():
+    # set critical parameters
+    seed = 1
+    folds = 10  # used in grid search
+    # the following model parameters will be fixed for now
+    epochs = 50
+    learning_rate = 1e-3
+    batch_size = 100
+    # setting miscellaneous parameters including those for I/O
     kernel_type = "rbf"
     output_pathname = "output"
     output_filepath = ospath.join(output_pathname, "out.csv")
@@ -55,24 +123,47 @@ def main():
     # Preprocessing step #1: Perform data scaling
     x_train_whitened, x_test_whitened = perform_data_scaling(x_train_orig, x_test_orig)
 
+    # Encode data and labels
+    y_train_encoded = encode_labels(y_train_orig)
+
     # PCA step #1
     pca = KernelPCA(kernel=kernel_type, n_components=2)
     x_pca = pca.fit_transform(x_train_whitened)
     plot3clusters(x_pca, y_train_orig, 'PCA', 'PC', "training_samples_{}.png".format(kernel_type))
 
-    # Preprocessing step #3/training: XGBoost
-    np.random.seed(1)
-
-    model = XGBClassifier(learning_rate=0.05, n_estimators=2, max_depth=5)
-    model.fit(x_train_whitened, y_train_orig)
+    ######################################
+    # grid search for optimal parameters #
+    ######################################
+    model = KerasClassifier(build_fn=create_model, epochs=epochs, learning_rate=learning_rate, batch_size=batch_size, verbose=False)
+    # neurons = [1000, 2500, 5000]
+    # layers = [2, 5, 8, 11]
+    # dropout = [0.3, 0.5, 0.7]
+    neurons = [10, 20]
+    layers = [1, 2]
+    dropout = [0.7, ]
+    param_grid = dict(npl=neurons, lrs=layers, dropout=dropout)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=folds)
+    grid_result = grid.fit(x_train_whitened, y_train_encoded)
+    # log the results
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    print('*' * 50)
+    print('mean (stdev) \t parameters')
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+    opt_npl, opt_lrs, opt_dropout = grid_result.best_params_['npl'], grid_result.best_params_['lrs'], grid_result.best_params_['dropout']
 
     # Do the prediction
-    y_predict = model.predict(x_test_whitened)
+    model = create_model(npl=opt_npl, lrs=opt_lrs, activation_fct='relu', dropout=opt_dropout)
+    model.fit(x_train_whitened, y_train_encoded, epochs=epochs, learning_rate=learning_rate, batch_size=batch_size, verbose=False)
+    model.summary()
+    y_test_predict = model.predict(x_test_whitened)
 
     # Prepare results dataframe
     results = np.zeros((x_test_orig.shape[0], 2))
     results[:, 0] = test_data_ids
-    results[:, 1] = y_predict
+    results[:, 1] = y_test_predict
 
     # save the output weights
     if not ospath.exists(output_pathname):
