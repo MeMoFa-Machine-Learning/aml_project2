026c0075df49e6641e70c062c0dadc0bf141df9e
Falko Julian Noe <falkonoe@gmail.com>
2019-11-11 00:55:02 +0100

    Implemented SVM with more grid search options

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import numpy as np
 from os import makedirs
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
-from sklearn.decomposition import PCA, KernelPCA
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-from xgboost import XGBClassifier
-import xgboost as xgb
 
 
 def perform_data_scaling(x_train, x_test):
@@ -25,21 +26,6 @@ def oversampling(X, y):
     noise = np.random.normal(0, 1, (X_res.shape[0] - X.shape[0], X.shape[1]))
     X_res[X.shape[0]:X_res.shape[0]] += noise
     return X_res, y_res
-
-
-def plot3clusters(x, y, title, vtitle, out_filename="training_data.png"):
-    plt.figure()
-    colors = ['navy', 'turquoise', 'darkorange']
-    lw = 2
-
-    for color, i, target_name in zip(colors, [0, 1, 2], [0, 1, 2]):
-        plt.scatter(x[y == i, 0], x[y == i, 1], color=color, alpha=.3, lw=lw,
-                  label=target_name)
-    plt.legend(loc='best', shadow=False, scatterpoints=1)
-    plt.title(title)
-    plt.xlabel(vtitle + "1")
-    plt.ylabel(vtitle + "2")
-    plt.savefig(out_filename)
 
 
 def main():
@@ -67,31 +53,35 @@ def main():
     # Preprocessing step #2: Oversampling
     x_res, y_res = oversampling(x_train_whitened, y_train_orig)
 
-    # PCA step #1
-    kernel_type = 'rbf'
-    pca = KernelPCA(kernel=kernel_type, n_components=7)
-    x_pca = pca.fit_transform(x_res)
-    plot3clusters(x_pca, y_res, 'PCA', 'PC', "training_samples_{}.png".format(kernel_type))
+    # Preprocessing step #3: Feature selection
+    n_components = [10, 100, 1000, ]
+    class_weight = [{0: 10, 1: 80, 2: 10}, {0: 12.5, 1: 75, 2: 12.5}]
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']
+    reg_param = list(np.logspace(start=-2, stop=2, num=5, endpoint=True, base=10))
 
-    # Preprocessing step #3/training: XGBoost
-    np.random.seed(1)
+    # n_components = [1000, ]
+    # class_weight = [{0: 25, 1: 75, 2: 25}, ]
+    # kernels = ['linear', ]
+    # reg_param = list(np.logspace(start=-2, stop=2, num=1, endpoint=True, base=10))
 
-    # model = XGBClassifier(learning_rate=0.05, n_estimators=2, max_depth=5)
-    # model.fit(x_res, y_res)
+    wclf = SVC(kernel='rbf', probability=True)
+    pca = PCA()
+    pipe = Pipeline(steps=[('pca', pca), ('wclf', wclf)])
+    param_grid = dict(
+        pca__n_components=n_components,
+        wclf__C=reg_param,
+        wclf__class_weight=class_weight,
+        wclf__kernel=kernels
+    )
+    kfold = StratifiedKFold(n_splits=3, shuffle=True, random_state=7)
 
-    X_train, X_eval, y_train, y_eval = train_test_split(x_pca, y_res, test_size=0.1, random_state=100)
-    dtrain = xgb.DMatrix(data=X_train, label=y_train)
-    deval = xgb.DMatrix(data=X_eval, label=y_eval)
-    dtest = xgb.DMatrix(pca.fit_transform(x_test_whitened))
-
-    param = {'max_depth': 3, 'eta': 1, 'objective': 'multi:softmax', 'num_class': 3, 'reg_alpha': 1, 'reg_lambda': 1,
-             'colsample_bytree': 0.3}
-    param['eval_metric'] = 'mlogloss'
-    evallist = [(deval, 'eval'), (dtrain, 'train')]
-    bst = xgb.train(dtrain=dtrain, params=param, evals=evallist, early_stopping_rounds=10, num_boost_round=500)
+    # C-support vector classification according to a one-vs-one scheme
+    grid_search = GridSearchCV(pipe, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold, verbose=0)
+    grid_result = grid_search.fit(x_res, y_res)
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
 
     # Do the prediction
-    y_predict = bst.predict(dtest)
+    y_predict = grid_search.predict(x_test_whitened)
     unique_elements, counts_elements = np.unique(y_predict, return_counts=True)
     print("test set labels and their corresponding counts")
     print(np.asarray((unique_elements, counts_elements)))
@@ -99,7 +89,7 @@ def main():
     # Confusion matrix
     # By definition a confusion matrix C is such that C_{i,j} is equal to the number of observations known to be in
     # group i but predicted to be in group j.
-    y_pred = bst.predict(xgb.DMatrix(pca.fit_transform(x_train_whitened)))
+    y_pred = grid_search.predict(x_train_whitened)
     y_true = y_train_orig
     print("training set confusion matrix")
     print(confusion_matrix(y_true, y_pred))
