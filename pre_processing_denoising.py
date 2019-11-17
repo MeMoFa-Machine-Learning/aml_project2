010deb726f451fc94a66691b4c468c147b52189d
Moritz Gück <moritzguck@student-net-cx-0926.ethz.ch>
2019-11-17 17:58:11 +0100

    implemented denoising and pca

--- pre_processing_denoising.py
+++ pre_processing_denoising.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2 as cv
 import os.path as ospath
 import pandas as pd
-
+from matplotlib import pyplot as plt
 
 # import data
 kernel_type = "rbf"
@@ -13,9 +13,18 @@ output_filepath = ospath.join(output_pathname, "out.csv")
 training_data_dir = ospath.join("data", "training")
 testing_data_dir = ospath.join("data", "testing")
 
-train_data_x = pd.read_csv(ospath.join(training_data_dir, "X_train.csv"), delimiter=",").drop(["id"], axis=1)
+train_data_x = pd.read_csv(ospath.join(training_data_dir, "X_train.csv"), delimiter=",")
 train_data_ids = train_data_x["id"]  # Get the ids for processing later on
 train_data_x = train_data_x.drop(["id"], axis=1)
+
+first_img = train_data_x.values[1]
+#first_img = np.reshape(first_img, (-1, 25))
+first_img = first_img*100
+first_img = first_img.astype(np.uint8)
+plt.imshow(first_img)
+denoised_img = cv.fastNlMeansDenoising(first_img, None, 1,7,7)
+plt.imshow(denoised_img)
+plt.show()
 
 test_data_x = pd.read_csv(ospath.join(testing_data_dir, "X_test.csv"), delimiter=",")
 test_data_ids = test_data_x["id"]  # Get the ids for processing later on
@@ -28,8 +37,8 @@ complete_table_train = np.zeros((train_data_x.shape[0]+1, train_data_x.shape[1]+
 
 
 # denoise image
-new_img_matrix = np.zeros(train_data_x.values.shape[0], train_data_x.values.shape[1])
-for index, image in enumerate(train_data_x):
-    denoised_img = cv.fastNlMeansDenoising(image,None,10,10,7,21) 
-    new_img_matrix[index] = denoised_img
-
+new_img_matrix = np.zeros((train_data_x.values.shape[0], train_data_x.values.shape[1]))
+for sample_index in range(0, train_data_x.values.shape[0]):
+    #denoised_img = cv.fastNlMeansDenoising(train_data_x.values[sample_index],None,10,10,7,21)
+    #new_img_matrix[sample_index] = denoised_img
+    pass
